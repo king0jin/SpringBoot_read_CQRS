f480baf43cad4bdbaafd3d65e141f260e623cfe2
king0jin <et0709@naver.com>
2024-12-07 16:03:34 +0900

    create read project

---
+++ src/main/java/naver/et0709/spring_cqrs_read/SpringCqrsReadApplication.java
@@ -0,0 +1,13 @@
+package naver.et0709.spring_cqrs_read;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCqrsReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCqrsReadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/naver/et0709/spring_cqrs_read/SpringCqrsReadApplicationTests.java
@@ -0,0 +1,13 @@
+package naver.et0709.spring_cqrs_read;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCqrsReadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
